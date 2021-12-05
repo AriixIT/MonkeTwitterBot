129a8c73e3c86de72ae91716c8845e94d6c2f42a
Amin <aminhaidarprivat@gmail.com>
2021-12-05 23:39:08 +0100

    changed directory

--- monkebot.py
+++ monkebot.py
@@ -13,7 +13,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-DIRECTORY = "D:\\programming\\Github\\MonkeTwitterBot\\pictures\\"
+DIRECTORY = "pictures\\"
 FILE_NAME = 'last_seen_id.txt'
 
 
